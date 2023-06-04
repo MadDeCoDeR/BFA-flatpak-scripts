0b448a787818096e2e0746cd6a25be182895cfd4
MadDeCoDeR <gkwin8ac@hotmail.gr>
2023-06-04 13:04:31 +0300

    Various updates and fixes

--- DOOMBFA.sh
+++ DOOMBFA.sh
@@ -6,8 +6,8 @@ elif [[ " $@ " =~ " -open-engine " ]]
 then
    mkdir -p "$HOME/.doombfa/base/lib"
    export LD_LIBRARY_PATH="$HOME/.doombfa/base/lib"
-   (cd "$HOME/.doombfa/base/lib" && exec /app/bin/DoomBFA $*)
+   (cd "$HOME/.doombfa/base/lib" && exec /app/bin/DoomBFA "$*")
 else
    mkdir -p "$HOME/.doombfa/base/lib"
-   mono /app/bin/CDL.exe $*
+   mono /app/bin/CDL.exe "$*"
 fi
