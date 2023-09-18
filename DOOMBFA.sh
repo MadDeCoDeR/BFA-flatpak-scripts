5ae9e648835c05eec4cde44216672e7315022749
MadDeCoDeR <gkwin8ac@hotmail.gr>
2023-09-18 22:47:59 +0300

    Add tag for the main source and fix the .desktop context menu

--- DOOMBFA.sh
+++ DOOMBFA.sh
@@ -10,5 +10,5 @@ then
 else
    mkdir -p "$HOME/.doombfa/base/lib"
    export LD_LIBRARY_PATH="$HOME/.doombfa/base/lib"
-   (cd "$HOME/.doombfa/base/lib" && exec /app/bin/DoomBFA "$*")
+   (cd "$HOME/.doombfa/base/lib" && exec /app/bin/DoomBFA $*)
 fi
